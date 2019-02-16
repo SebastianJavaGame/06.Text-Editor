3f7b2e495c45af8f5beef6640e43bff127c9f3f3
protected300 <sebek2088s4@tlen.pl>
2019-02-16 10:53:36 +0100

    Remove duplicate and repair fail

--- 06.Text-Editor-master/src/scislak/program/App.java
+++
@@ -1,9 +0,0 @@
-package scislak.program;
-
-public class App {
-
-	public static void main(String[] args) {
-		Frame.getInstance().init();
-	}
-
-}
--- src/scislak/program/Frame.java
+++ src/scislak/program/Frame.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.print.PrinterException;
 
 import javax.swing.JFrame;
 import javax.swing.JMenu;
@@ -232,7 +233,12 @@ public class Frame{
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				new PrintFile();
+				try {
+					new PrintFile();
+				} catch (PrinterException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 			}
 		});
 	}
